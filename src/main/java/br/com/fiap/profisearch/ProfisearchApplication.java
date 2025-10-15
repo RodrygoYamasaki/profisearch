a4e20642184837b7fb2eeda0763915ef8426f89c
RodrygoYamasaki <rodrygo.yamasaki@gmail.com>
2025-10-15 12:50:43 -0300

    Add initial project structure

---
+++ src/main/java/br/com/fiap/profisearch/ProfisearchApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.profisearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfisearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfisearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/profisearch/ProfisearchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.profisearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfisearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
